ffd30995966a89ef3ffc5b2e8164fec544a97e05
Drevarr <drevarr@gmail.com>
2026-09-20 15:30:14 -0400

    update version

--- _version.py
+++ _version.py
@@ -1,2 +1,2 @@
-VERSION = "1.9.0"
+VERSION = "1.9.2"
 __version__ = VERSION
--- tw5_top_stats.py
+++ tw5_top_stats.py
@@ -30,7 +30,7 @@ from parser_functions import *
 from output_functions import *
 from fight_review import *
 
-CURRENT_VERSION = "1.9.0"  
+CURRENT_VERSION = "1.9.2"  
 REPO = "Drevarr/GW2_EI_log_combiner"
 LATEST_VERSION = None
 
